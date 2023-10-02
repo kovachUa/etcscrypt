2684af15033323926134461c104eea41c496f758
Kovach <121579290+kovachUa@users.noreply.github.com>
2023-10-02 14:08:45 -0400

    Update ZFS_snapshots.sh

--- zfs/ZFS_snapshots.sh
+++ zfs/ZFS_snapshots.sh
@@ -3,31 +3,27 @@
 # Задайте список пулів та файлових систем, для яких потрібно створювати снапшоти
 pool_list=("zfs/pool")
 
-# Задайте кількість днів, старше яких снапшоти будуть видалятися
-retention_days=7
+# Кількість днів, для яких потрібно зберігати снапшоти
+DAYS_TO_KEEP=3
 
-# Отримати поточну дату
-current_date=$(date +%d.%m.%Y)
+# Створення снапшотів
+for POOL_NAME in "${POOL_NAMES[@]}"; do
+  # Створення снапшоту з поточною датою
+  CURRENT_DATE=$(date +%d.%m.%Y)
+  SNAPSHOT_NAME="$POOL_NAME@$CURRENT_DATE"
+  zfs snapshot "$SNAPSHOT_NAME"
+  echo "Створено снапшот: $SNAPSHOT_NAME"
+done
 
-# Пройти по кожному пулу та файловій системі та створити снапшоти
-for pool_name in "${pool_list[@]}"; do
-  # Отримати список файлових систем для цього пулу
-  filesystems=($(zfs list -H -o name -r $pool_name))
-
-  for filesystem in "${filesystems[@]}"; do
-    # Створити снапшот для кожної файлової системи
-    snapshot_name="$filesystem@$current_date"
-    zfs snapshot $snapshot_name
-
-    # Видалити старі снапшоти, старші за retention_days
-    if [ $retention_days -gt 0 ]; then
-      oldest_date=$(date -d "-$retention_days days" +%d.%m.%Y)
-      zfs list -t snapshot -o name -H -r $filesystem | while read -r snapshot; do
-        snapshot_date=$(echo $snapshot | awk -F "@" '{print $2}')
-        if [[ $snapshot_date < $oldest_date ]]; then
-          zfs destroy $snapshot
-        fi
-      done
+# Видалення старших снапшотів
+for POOL_NAME in "${POOL_NAMES[@]}"; do
+  # Видалення снапшотів, які старші за DAYS_TO_KEEP днів
+  zfs list -t snapshot -o name,creation -Hr "$POOL_NAME" | while read -r snapshot creation; do
+    snapshot_date=$(date -d "$creation" +%s)
+    cutoff_date=$(date -d "$DAYS_TO_KEEP days ago" +%s)
+    if [ "$snapshot_date" -lt "$cutoff_date" ]; then
+      zfs destroy "$snapshot"
+      echo "Видалено старший снапшот: $snapshot"
     fi
   done
 done
