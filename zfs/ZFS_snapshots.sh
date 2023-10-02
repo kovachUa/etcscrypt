2194ab9e33f42363cd5f1b359f566da814371a9a
Kovach <121579290+kovachUa@users.noreply.github.com>
2023-10-02 13:01:43 -0400

    Update ZFS_snapshots.sh

--- zfs/ZFS_snapshots.sh
+++ zfs/ZFS_snapshots.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Задайте список пулів, для яких потрібно створювати снапшоти
+# Задайте список пулів та файлових систем, для яких потрібно створювати снапшоти
 pool_list=("zfs/pool")
 
 # Задайте кількість днів, старше яких снапшоти будуть видалятися
@@ -9,24 +9,27 @@ retention_days=7
 # Отримати поточну дату
 current_date=$(date +%d.%m.%Y)
 
-# Пройти по кожному пулу та створити снапшоти
+# Пройти по кожному пулу та файловій системі та створити снапшоти
 for pool_name in "${pool_list[@]}"; do
-  # Створити снапшоти для файлової системи ZFS
-  snapshot_name="$pool_name@$current_date"
-  zfs snapshot $snapshot_name
+  # Отримати список файлових систем для цього пулу
+  filesystems=($(zfs list -H -o name -r $pool_name))
 
-  # Видалити старі снапшоти, старші за retention_days
-  if [ $retention_days -gt 0 ]; then
-    zfs list -t snapshot -o name -H -r $pool_name | while read -r snapshot; do
-      snapshot_date=$(echo $snapshot | awk -F "@" '{print $2}')
-      snapshot_date_seconds=$(date --date="$snapshot_date" +%s 2>/dev/null)
-      current_date_seconds=$(date --date="$current_date" +%s 2>/dev/null)
-      retention_seconds=$((retention_days * 24 * 60 * 60))
-      if [ "$snapshot_date_seconds" != "" ] && [ "$current_date_seconds" != "" ] && [ $((current_date_seconds - snapshot_date_seconds)) -gt $retention_seconds ]; then
-        zfs destroy $snapshot
-      fi
-    done
-  fi
+  for filesystem in "${filesystems[@]}"; do
+    # Створити снапшот для кожної файлової системи
+    snapshot_name="$filesystem@$current_date"
+    zfs snapshot $snapshot_name
+
+    # Видалити старі снапшоти, старші за retention_days
+    if [ $retention_days -gt 0 ]; then
+      oldest_date=$(date -d "-$retention_days days" +%d.%m.%Y)
+      zfs list -t snapshot -o name -H -r $filesystem | while read -r snapshot; do
+        snapshot_date=$(echo $snapshot | awk -F "@" '{print $2}')
+        if [[ $snapshot_date < $oldest_date ]]; then
+          zfs destroy $snapshot
+        fi
+      done
+    fi
+  done
 done
 
 echo "Скрипт успішно виконано!"
